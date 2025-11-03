749b80b3dffa21d5b0bd46ec6a8ff16f2f9e3bbf
Oliver Wieland <oliver.wieland@unitybox.de>
2025-11-03 19:49:04 +0100

    feat: Add unit test support with doctest and refactor test files

--- libdoipserver/test/RoutingActivationHandler_Test.cpp
+++ libdoipserver/test/RoutingActivationHandler_Test.cpp
@@ -1,7 +1,6 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
 #include "RoutingActivationHandler.h"
 #include <stdint.h>
+#include <doctest/doctest.h>
 
 TEST_SUITE("RoutingActivation") {
 	struct RoutingActivationFixture {
--- libdoipserver/test/VehicleIdentificationHandler_Test.cpp
+++ libdoipserver/test/VehicleIdentificationHandler_Test.cpp
@@ -1,81 +1,85 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include <doctest/doctest.h>
+#include "DoIPServer.h"
 #include "VehicleIdentificationHandler.h"
+#include <doctest/doctest.h>
 #include <stdint.h>
 #include <string>
-#include "DoIPServer.h"
 
 using namespace std;
 
 TEST_SUITE("VehicleIdentificationHandler") {
-	struct VehicleIdentificationHandlerFixture {
-		string matchingVIN = "MatchingVin_12345";
-		string shortVIN = "shortVin";
-		string shortVINPadded = "shortVin000000000";
-		unsigned char EID[6] = {0, 0, 0, 0, 0, 0};
-		unsigned char GID[6] = {0, 0, 0, 0, 0, 0};
-		unsigned char far = 0;
+    struct VehicleIdentificationHandlerFixture {
+        string matchingVIN = "MatchingVin_12345";
+        string shortVIN = "shortVin";
+        string shortVINPadded = "shortVin000000000";
+        unsigned char EID[6] = {0, 0, 0, 0, 0, 0};
+        unsigned char GID[6] = {0, 0, 0, 0, 0, 0};
+        unsigned char far = 0;
 
-		VehicleIdentificationHandlerFixture() {
-			// Setup code here if needed
-		}
+        VehicleIdentificationHandlerFixture() {
+            // Setup code here if needed
+        }
 
-		~VehicleIdentificationHandlerFixture() {
-			// Cleanup code here if needed
-		}
-	};
+        ~VehicleIdentificationHandlerFixture() {
+            // Cleanup code here if needed
+        }
+    };
 
-	/*
-	 * Checks if a VIN with 17 bytes matches correctly the input data
-	 */
-	TEST_CASE_FIXTURE(VehicleIdentificationHandlerFixture, "VIN 17 Bytes") {
-		// Call function under test to create message
-		unsigned char * message = createVehicleIdentificationResponse(matchingVIN, 0, EID, GID, far);
+    /*
+     * Checks if a VIN with 17 bytes matches correctly the input data
+     */
+    TEST_CASE_FIXTURE(VehicleIdentificationHandlerFixture, "VIN 17 Bytes") {
+        // Call function under test to create message
+        unsigned char *message =
+            createVehicleIdentificationResponse(matchingVIN, 0, EID, GID, far);
 
-		// Extract VIN from created test message
-		char tempvin[18]; // Need 1 byte more for \0 at the end for parsing via string()
-		for(int i=0; i<=16; i++) {
-			tempvin[i] = message[i+8];
-		}
-		tempvin[17] = 0; // Mark end of string
+        // Extract VIN from created test message
+        char tempvin[18]; // Need 1 byte more for \0 at the end for parsing via
+                          // string()
+        for (int i = 0; i <= 16; i++) {
+            tempvin[i] = message[i + 8];
+        }
+        tempvin[17] = 0; // Mark end of string
 
-		delete[] message;
+        delete[] message;
 
-		// Assert that extracted VIN matches input data
-		string expected = string(tempvin);
-		CHECK_EQ(matchingVIN, expected);
-		CHECK_MESSAGE(matchingVIN == expected, "Setting VIN with 17 bytes failed");
-	}
+        // Assert that extracted VIN matches input data
+        string expected = string(tempvin);
+        CHECK_EQ(matchingVIN, expected);
+        CHECK_MESSAGE(matchingVIN == expected, "Setting VIN with 17 bytes failed");
+    }
 
-	/*
-	 * Checks if a VIN < 17 bytes is padded correctly with zero bytes
-	 */
-	TEST_CASE_FIXTURE(VehicleIdentificationHandlerFixture, "VIN Less Than 17 Bytes") {
-		// Call function under test to create message
-		unsigned char * message = createVehicleIdentificationResponse(shortVIN, 0, EID, GID, far);
+    /*
+     * Checks if a VIN < 17 bytes is padded correctly with zero bytes
+     */
+    TEST_CASE_FIXTURE(VehicleIdentificationHandlerFixture,
+                      "VIN Less Than 17 Bytes") {
+        // Call function under test to create message
+        unsigned char *message =
+            createVehicleIdentificationResponse(shortVIN, 0, EID, GID, far);
 
-		// Extract VIN from created test message
-		char c_tempvin[18];
-		for(int i=0; i<=16; i++) {
-			c_tempvin[i] = message[i+8];
-		}
-		c_tempvin[17] = 0; // Mark end of string
+        // Extract VIN from created test message
+        char c_tempvin[18];
+        for (int i = 0; i <= 16; i++) {
+            c_tempvin[i] = message[i + 8];
+        }
+        c_tempvin[17] = 0; // Mark end of string
 
-		// Create expected string value
-		string actualVin = string(c_tempvin);
+        // Create expected string value
+        string actualVin = string(c_tempvin);
 
-		// Assert that extracted VIN matches input data
-		CHECK_EQ(shortVINPadded, actualVin);
-		CHECK_MESSAGE(shortVINPadded == actualVin, "Setting VIN with < 17 bytes failed");
+        // Assert that extracted VIN matches input data
+        CHECK_EQ(shortVINPadded, actualVin);
+        CHECK_MESSAGE(shortVINPadded == actualVin,
+                      "Setting VIN with < 17 bytes failed");
 
-		// Assert message after shortVin bytes is padded with '0'
-		for(int i=16; i<=24; i++) {
-			CHECK_EQ(message[i], '0');
-			if (message[i] != '0') {
-				CHECK_MESSAGE(false, "VIN not correctly padded at byte: " + std::to_string(i));
-			}
-		}
-		delete[] message;
-	}
+        // Assert message after shortVin bytes is padded with '0'
+        for (int i = 16; i <= 24; i++) {
+            CHECK_EQ(message[i], '0');
+            if (message[i] != '0') {
+                CHECK_MESSAGE(false,
+                              "VIN not correctly padded at byte: ", std::to_string(i));
+            }
+        }
+        delete[] message;
+    }
 }
-
