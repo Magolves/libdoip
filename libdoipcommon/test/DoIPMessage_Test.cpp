0ab54c9f35a731214a813f77fa4612b887767212
Oliver Wieland <oliver.wieland@unitybox.de>
2025-11-04 20:14:01 +0100

    test: Refactor DoIPMessage test for improved clarity and consistency

--- libdoipcommon/test/DoIPMessage_Test.cpp
+++ libdoipcommon/test/DoIPMessage_Test.cpp
@@ -6,7 +6,13 @@ using namespace doip;
 
 TEST_SUITE("DoIPMessage") {
     TEST_CASE("Message assembly") {
-        DoIPMessage msg(DoIPPayloadType::AliveCheckRequest, {0x01, 0x02} );
+        DoIPMessage msg(DoIPPayloadType::AliveCheckRequest, {0x01, 0x02});
+        ByteArray expected{
+            0x04, 0xfb,             // protocol version + inv
+            0x00, 0x07,             // payload type
+            0x00, 0x00, 0x00, 0x02, // payload length
+            0x01, 0x02              // payload
+        };
 
         CHECK(msg.getPayloadSize() == 2);
         CHECK(msg.getMessageSize() == 10);
@@ -14,8 +20,8 @@ TEST_SUITE("DoIPMessage") {
 
         const auto bytes = msg.toBytes();
 
-        CHECK(bytes[0] == 4);
-        CHECK(bytes[1] == 0xFB);
-
+        for (size_t i = 0; i < bytes.size(); i++) {
+            CHECK_MESSAGE(bytes.at(i) == expected.at(i), "Bytes to not match at pos ", i, ", got ", bytes.at(i), ", expected ", expected.at(i));
+        }
     }
 }
